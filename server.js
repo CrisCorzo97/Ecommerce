1b99b369aba3775b2c3e335ac4a614c87393a8e8
CrisCorzo <CrCorzo04@hotmail.com>
2021-12-02 21:29:38 -0300

    config db and server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
-const cookieParser = require('cooke-parser');
+const cookieParser = require('cookie-parser');
 
 
 const app = express();
@@ -13,3 +13,18 @@ app.use(cors());
 app.use(fileUpload({
     useTempFiles: true,
 }));
+
+//Connect to mongodb
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, err => {
+    if(err) throw err;
+    console.log('Connected to MongoDB');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT)
+});
