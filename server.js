5e57295efc4bc133d70f364bae55ae9f08e7141b
CrisCorzo97 <CrCorzo04@hotmail.com>
2021-12-05 18:34:47 -0300

    back-end / CategoryModel 1/2

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(fileUpload({
 }));
 
 //Routes
-app.use('/user', require('./routes/userRouter'))
+app.use('/user', require('./routes/userRouter'));
+app.use('/api', require('./routes/categoryRouter'));
 
 //Connect to mongodb
 const URI = process.env.MONGODB_URL;
